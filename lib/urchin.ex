600b4d385d84d35dcd147d77158a4e117622a437
Dax Ravi <d@ironbay.co>
2018-03-09 19:07:33 -0500

    Fixed bug

--- lib/urchin.ex
+++ lib/urchin.ex
@@ -32,10 +32,10 @@ defmodule Urchin do
           _ -> {:error, error}
         end
       end)
-      |> Enum.find_value(fn
-        result = {:error, _} -> result
-        _ -> :ok
-      end)
+      |> Enum.find(:ok, fn
+          result = {:error, _} -> true
+          _ -> false
+        end)
     end
   end
 
@@ -60,16 +60,29 @@ defmodule Urchin.Example do
   import Urchin
 
   def run() do
-    data = %{
-      "lol" => "",
-      "some" => %{
-        "data" => "ok"
+    %{
+      "accessibility" => %{"service" => "basic", "wheelchair" => false},
+      "address" => %{
+        "format" => "380 Rector Place, New York, NY, USA",
+        "lat" => 40.7090972,
+        "lng" => -74.01775609999999,
+        "name" => "380 Rector Pl",
+        "raw" => "380 Rector Pl, New York, NY 10280, USA"
       },
+      "communication" => %{"has_mobile" => false},
+      "created" => 1520606988817,
+      "key" => "0QloFs3iIwuAkvOstFn9",
+      "language" => "en",
+      "name" => %{"first" => "Dax", "last" => ""},
+      "timezone" => "America/New_York"
     }
-
-    validate data do
-      field: ["lol"], tests: [Urchin.string(1, 10)] -> "lol is invalid"
-      field: ["some", "data"], tests: [Urchin.required()] -> "Some data is invalid"
+    |> validate do
+			field: ["name", "first"], tests: [Urchin.string(1)] -> "First name is invalid"
+			field: ["name", "last"], tests: [Urchin.string(1)] -> "Last name is invalid"
+			field: ["timezone"], tests: [Urchin.required()] -> "Timezone is invalid"
+			field: ["address", "raw"], tests: [Urchin.required()] -> "Address is invalid"
+			field: ["language"], tests: [Urchin.required()] -> "Language is invalid"
+			field: ["communication", "has_mobile"], tests: [Urchin.required()] -> "Please specify if patient has a mobile number"
     end
   end
 end
