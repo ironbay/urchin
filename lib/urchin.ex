b03aa93461e98f0a7037fac7e21d0ac488f71366
Dax Ravi <d@ironbay.co>
2018-03-09 19:17:11 -0500

    Better data

--- lib/urchin.ex
+++ lib/urchin.ex
@@ -62,13 +62,6 @@ defmodule Urchin.Example do
   def run() do
     %{
       "accessibility" => %{"service" => "basic", "wheelchair" => false},
-      "address" => %{
-        "format" => "380 Rector Place, New York, NY, USA",
-        "lat" => 40.7090972,
-        "lng" => -74.01775609999999,
-        "name" => "380 Rector Pl",
-        "raw" => "380 Rector Pl, New York, NY 10280, USA"
-      },
       "communication" => %{"has_mobile" => false},
       "created" => 1520606988817,
       "key" => "0QloFs3iIwuAkvOstFn9",
@@ -80,7 +73,6 @@ defmodule Urchin.Example do
 			field: ["name", "first"], tests: [Urchin.string(1)] -> "First name is invalid"
 			field: ["name", "last"], tests: [Urchin.string(1)] -> "Last name is invalid"
 			field: ["timezone"], tests: [Urchin.required()] -> "Timezone is invalid"
-			field: ["address", "raw"], tests: [Urchin.required()] -> "Address is invalid"
 			field: ["language"], tests: [Urchin.required()] -> "Language is invalid"
 			field: ["communication", "has_mobile"], tests: [Urchin.required()] -> "Please specify if patient has a mobile number"
     end
